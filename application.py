d03f69e0937e8ebb2857afda81730b5be5f62799
Mathew <24691566+mesporas1@users.noreply.github.com>
2019-04-05 17:59:22 -0700

    Testing javascript out..

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import requests
 from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO, emit
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
@@ -15,7 +15,7 @@ votes = {"yes": 0, "no": 0, "maybe": 0}
 def index():
     return render_template("index.html")
 
-
+    
 @socketio.on("submit vote")
 def vote(data):
     selection = data["selection"]
--- static/index.js
+++ static/index.js
@@ -1,14 +1,2 @@
-if (!localStorage.getItem('user'))
-    localStorage.setItem('user', "");
-
-// Load current value of  counter
-document.addEventListener('DOMContentLoaded', () => {
-document.querySelector('#form').innterHTML = localStorage.getItem('user');
-
-// Count every time button is clicked
-    document.querySelector('form').onsubmit = () => {
-    let user = localStorage.getItem('user')
-    localStorage.setItem('user', user)
-    alert(`Hello ${user}!`)
-    }
-});
+function hello(){
+alert('Hello World');}
